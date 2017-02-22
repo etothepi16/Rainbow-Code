3bfa1693dd5c8e0b8aacf04d902e51890572b723
etothepi16 <paulmcdowell18@gmail.com>
2017-02-21 22:31:05 -0500

    wings now rotate

--- challenges/CC_60_Butterfly_Wings/sketch.js
+++ challenges/CC_60_Butterfly_Wings/sketch.js
@@ -4,6 +4,7 @@
 // Video: [coming soon]
 
 var yoff = 0;
+var angle = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -12,8 +13,7 @@ function setup() {
 function draw() {
   background(51);
   translate(width / 2, height / 2);
-  rotate(PI / 2);
-
+  rotate(angle);
   stroke(255);
   fill(255, 50);
   strokeWeight(1);
@@ -23,17 +23,7 @@ function draw() {
 
   var xoff = 0;
   beginShape();
-  for (var a = -PI / 2; a <= PI / 2; a += da) {
-    var n = noise(xoff, yoff);
-    var r = sin(2 * a) * map(n, 0, 1, 50, 300);
-    var x = r * cos(a);
-    var y = r * sin(a);
-    xoff += dx;
-    //point(x, y);
-    vertex(x, y);
-  }
-
-  for (var a = PI / 2; a <= 3 * PI / 2; a += da) {
+  for (var a = -PI / 2; a <= 3 * PI / 2; a += da) {
     var n = noise(xoff, yoff);
     var r = sin(2 * a) * map(n, 0, 1, 50, 300);
     var x = r * cos(a);
@@ -45,4 +35,5 @@ function draw() {
   endShape();
 
   yoff += 0.01;
+  angle += PI / 240;
 }
